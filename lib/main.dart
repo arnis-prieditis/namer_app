761d9a399d614df7b1f3a23b051bad998fb1cfec
arnis-prieditis <arnis.prieditis19@gmail.com>
2024-07-12 18:07:23 +0300

    change color and center widgets

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
         title: 'Namer App',
         theme: ThemeData(
           useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightGreen),
         ),
         home: MyHomePage(),
       ),
@@ -39,19 +39,55 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
+    var pair = appState.current;
 
     return Scaffold(
-      body: Column(
-        children: [
-          Text('A random idea:'),
-          Text(appState.current.asLowerCase),
-          ElevatedButton(
-            onPressed: () {
-              appState.getNext();
-            },
-            child: Text("Next"),
-          )
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            BigName(pair: pair),
+            SizedBox(height: 15,),
+            ElevatedButton(
+              onPressed: () {
+                appState.getNext();
+              },
+              child: Text("Next"),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class BigName extends StatelessWidget {
+  const BigName({
+    super.key,
+    required this.pair,
+  });
+
+  final WordPair pair;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
+      fontWeight: FontWeight.bold,
+    );
+
+    return Card(
+      color: theme.colorScheme.primary,
+      shadowColor: theme.colorScheme.surface,
+      elevation: 15,
+      child: Padding(
+        padding: const EdgeInsets.all(30),
+        child: Text(
+          pair.asLowerCase,
+          style: style,
+          semanticsLabel: "${pair.first} ${pair.second}",
+        ),
       ),
     );
   }
